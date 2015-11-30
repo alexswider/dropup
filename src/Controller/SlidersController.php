30a9229eb525fd4b48da7280b51649bbc695f9e6
Marcin Kozak <marcinkozakk@gmail.com>
2015-11-30 12:22:13 +0100

    hot fix: delete dir in dir fatal error

--- src/Controller/SlidersController.php
+++ src/Controller/SlidersController.php
@@ -385,7 +385,7 @@ class  SlidersController extends AppController
          $objects = scandir($dir); 
          foreach ($objects as $object) { 
            if ($object != "." && $object != "..") { 
-             if (filetype($dir."/".$object) == "dir") rrmdir($dir."/".$object); else unlink($dir."/".$object); 
+             if (filetype($dir."/".$object) == "dir") $this->rrmdir($dir."/".$object); else unlink($dir."/".$object); 
            } 
          } 
          reset($objects); 
